b033163b033c73f964410cd8f91608fd800d5e56
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2021-10-25 22:13:18 -0300

    New tables

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -30,6 +30,71 @@ ALTER TABLE `access_profile`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- Estrutura `institution`
+--
+
+CREATE TABLE `institution` (
+    `id` int(11) NOT NULL,
+    `institution` varchar(255) NOT NULL,
+    `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_at`timestamp ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes `institution`
+--
+ALTER TABLE `institution`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT `institution`
+--
+ALTER TABLE `institution`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Estrutura `national_mobility`
+--
+
+CREATE TABLE `national_mobility` (
+    `id` int(11) NOT NULL,
+    `first_name` varchar(200) NOT NULL,
+    `last_name` varchar(200) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `phone` varchar(15) NOT NULL,
+    `course` varchar(255) NOT NULL,
+    `semester` int(11) NOT NULL,
+    `id_home_institution` int(11) NOT NULL,
+    `id_destination_institution` int(11) NOT NULL,
+    `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_at`timestamp ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes `national_mobility`
+--
+ALTER TABLE `national_mobility`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `national_mobility`
+  ADD CONSTRAINT `fk_home_institution`
+  FOREIGN KEY (`id_home_institution`)
+  REFERENCES `institution` (`id`)
+  ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE `national_mobility`
+  ADD CONSTRAINT `fk_destination_institution`
+  FOREIGN KEY (`id_destination_institution`)
+  REFERENCES `institution` (`id`)
+  ON UPDATE CASCADE ON DELETE RESTRICT;
+
+--
+-- AUTO_INCREMENT `national_mobility`
+--
+ALTER TABLE `national_mobility`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Estrutura `member`
 --
 
@@ -60,4 +125,31 @@ ALTER TABLE `member`
 -- AUTO_INCREMENT `member`
 --
 ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Estrutura `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `theme` varchar(200) NOT NULL,
+  `local` varchar(200) NOT NULL,
+  `visible` int(1) NOT NULL,
+  `category` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at`timestamp ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT `event`
+--
+ALTER TABLE `event`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
--- sql/insert-into-access-profile.sql
+++ sql/insert-into-access-profile.sql
@@ -1,3 +1,5 @@
 USE g7;
 
 INSERT INTO `access_profile` (`access_profile`) VALUE ('Básico'), ('Visualizador'), ('Explorador'), ('Administrador');
+
+INSERT INTO `institution` (`institution`) VALUE ('Eniac | Centro Universitário'), ('Faculdade FATEB'), ('Faculdade FECAF'), ('FAESA | Centro Universitário'), ('Grupo Unis | Cidade Universitária'), ('Toledo Prudente | Centro Universitário'), ('UNDB | Centro Universitário'), ('UniOpet | Centro Universitário'), ('UNISUAM | Centro Universitário');
